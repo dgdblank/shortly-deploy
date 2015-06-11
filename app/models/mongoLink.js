d90ad97f9c331a95f0d22db067010969ee2b2af6
Hack Reactor Students <accounts@hackreactor.com>
2015-06-11 11:10:39 -0700

    Refactor link models for MongoDB

--- app/models/mongoLink.js
+++ app/models/mongoLink.js
@@ -1,14 +1,26 @@
 var mongoose = require('mongoose');
+var crypto = require('crypto');
 var Schema = mongoose.Schema;
+
+var encrypt = function(url) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(url);
+  return shasum.digest('hex').slice(0, 5);
+};
 
 var linkSchema = new Schema({
   url: String,
   base_url: String,
   code: String,
   title: String,
-  visits: Number
-})
+  visits: {type: Number, default: 0}
+});
+
+linkSchema.pre('save', function(next) {
+  this.code = encrypt(this.url);
+  next();
+});
 
 var Link = mongoose.model('Link', linkSchema);
 
-module.exports.Link = Link;
+module.exports = Link;
